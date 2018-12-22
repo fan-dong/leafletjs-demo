393261e5cbadfcd9f7ad51f0c9e94611631e09c2
Fred <911651855@qq.com>
2018-12-22 21:06:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# leafletjs-demo
+
+> 一个适用于移动设备的交互式地图的开源JavaScript库
+> 官网：https://leafletjs.com/
+
+## 示意图
+
+![示意图](static/eg.png)
---
+++ src/components/map.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="map">
+    <div class="mapBox" id="mapBox"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    let map = L.map("mapBox", {
+      attributionControl: false,
+      zoomControl: false,
+      center: [29.791133, 121.992123],
+      zoom: 13
+    });
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09])
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.map
+  width 100%
+  height 100%
+  .mapBox
+    width 100%
+    height 100%
+</style>
+
